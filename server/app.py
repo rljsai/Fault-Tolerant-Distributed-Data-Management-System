f359a4a58ce4565860d8c81c26041c1e697e1434
rljsai <leelajogeendarsaireddi@gmail.com>
2025-09-02 18:55:51 +0530

    server part completed including it's docker part

---
+++ server/app.py
@@ -0,0 +1,24 @@
+from flask import Flask ,jsonify
+import os
+
+app=Flask(__name__)
+
+SERVER_ID =os.environ.get("SERVER_ID","default-server")
+
+
+@app.route("/home",methods=["GET"])
+def home():
+    return jsonify({
+        "message": f"Hello from server: {SERVER_ID}",
+        "status":"successful"
+    })
+
+@app.route("/heartbeat",methods=["GET"])
+def heartbeat():
+    return jsonify({
+        "message":"active",
+        "status":"successful"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000)
